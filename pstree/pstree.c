ac6c3a14dabf43b104a505f8484b1a6863b8d118
tracer-nju <tracer@nju.edu.cn>
2023-03-10 10:32:03 +0800

    Linux LAPTOP-HFSOU9IB 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  10:32:03 up 46 min,  0 users,  load average: 0.09, 0.06, 0.05

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -37,6 +37,7 @@ void insert_all()
             // struct dirent *dpid;
             char status_name[256], buf[256];
             sprintf( status_name,"/proc/%d/status", pid);
+            printf("%s", status_name);
             FILE *fstatus = fopen(status_name, O_RDONLY);
             if(!fstatus)handle_error("open status");
             
