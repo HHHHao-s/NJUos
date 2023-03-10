14f508505eabe0a4d3dc0bef0a84f736972af1cc
tracer-nju <tracer@nju.edu.cn>
2023-03-10 10:23:22 +0800

    Linux LAPTOP-HFSOU9IB 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  10:23:22 up 37 min,  0 users,  load average: 0.12, 0.10, 0.07

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -21,13 +21,13 @@ void insert_all()
     if(dir==NULL)handle_error("opendir");
     while((d = readdir(dir)) != NULL){
         char *pid_c = d->d_name;
-        printf("%s\n", pid_c);
+        // printf("%s\n", pid_c);
         int pid=0;
 
         for(;*pid_c;pid_c++){
             if(*pid_c>'0' && *pid_c<'9') {
                 pid = pid*10 + *pid_c-'0';
-                printf("%d",pid);
+                printf("%d\n",pid);
             }
             
             else break;
