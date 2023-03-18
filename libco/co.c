315d829210f20cc579747e756e8cb09baaae6875
tracer-nju <tracer@nju.edu.cn>
2023-03-18 13:44:16 +0800

    Linux LAPTOP-HFSOU9IB 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  13:44:16 up  2:33,  0 users,  load average: 0.00, 0.02, 0.00

--- libco/co.c
+++ libco/co.c
@@ -147,7 +147,7 @@ void co_yield()
         // pick one routinue to run
         struct co * next=pick();
         if(next->status == CO_NEW){
-            
+            next->status = CO_RUNNING;
             next->retfun = co_finish; 
             stack_switch_call(&next->stack[STACKSIZE],next->func, (uintptr_t)next->arg); // 数据结构在堆上申请，低地址是结构的第一个参数，而栈是向下增长，所以要用高地址作为栈顶
         }
