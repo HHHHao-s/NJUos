c9fb22215f97e44fe2f2180d5bf5559c908834f4
tracer-nju <tracer@nju.edu.cn>
2023-03-18 15:28:26 +0800

    Linux LAPTOP-HFSOU9IB 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:28:26 up  4:17,  0 users,  load average: 0.00, 0.01, 0.00

--- libco/co.c
+++ libco/co.c
@@ -180,7 +180,7 @@ static void co_finish()
 
 void co_print(){
     for(int i=0;i<q.size;i++){
-        printf("name=%s\n status=%s", q.array[i]->name, q.array[i]->status);
+        printf("name=%s\n status=%d", q.array[i]->name, q.array[i]->status);
     }
 }
 
