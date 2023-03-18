4f94a42916ec80a4ca26df0f36d85b913a7471f5
tracer-nju <tracer@nju.edu.cn>
2023-03-18 15:46:57 +0800

    Linux LAPTOP-HFSOU9IB 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:46:57 up  4:36,  0 users,  load average: 0.02, 0.03, 0.00

--- libco/co.c
+++ libco/co.c
@@ -201,7 +201,7 @@ void co_yield()
         // pick one routinue to run
         struct co * next=pick();
         
-        printf("cur:%s cur->status:%d next:%s next->status:%s\n", current->name, current->status, next->name, next->status );
+        printf("cur:%s cur->status:%d next:%s next->status:%d\n", current->name, current->status, next->name, next->status );
         current=next;
         if(next->status == CO_NEW){
             
