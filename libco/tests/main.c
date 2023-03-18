20226de7685825bf86ca886d2c703cee6db904bf
tracer-nju <tracer@nju.edu.cn>
2023-03-18 12:36:42 +0800

    Linux LAPTOP-HFSOU9IB 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  12:36:42 up  1:26,  0 users,  load average: 0.00, 0.01, 0.00

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -17,7 +17,7 @@ static int get_count() {
 static void work_loop(void *arg) {
     const char *s = (const char*)arg;
     for (int i = 0; i < 2; ++i) {
-        // puts(s);
+        
         printf("%d", get_count());
         add_count();
         co_yield();
