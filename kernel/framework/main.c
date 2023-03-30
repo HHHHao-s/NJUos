7bf2f12601bea5dae32e0e5b2f4180aea33603da
tracer-nju <tracer@nju.edu.cn>
2023-03-30 20:47:59 +0800

    Linux LAPTOP-HFSOU9IB 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  20:47:59 up 13 min,  0 users,  load average: 0.20, 0.10, 0.05

--- kernel/framework/main.c
+++ kernel/framework/main.c
@@ -5,8 +5,8 @@ int main() {
   os->init();
   printf("mother fucker\n");
   mpe_init(os->run);
-  void *p=pmm->alloc(1000);
-  printf("%p", p);
-  free(p);
+  // void *p=pmm->alloc(1000);
+  // printf("%p", p);
+  // free(p);
   return 1;
 }
