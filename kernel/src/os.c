d665a0d5a2a866b0bf1721245db282efcb7ba62f
tracer-nju <tracer@nju.edu.cn>
2023-04-01 10:50:54 +0800

    Linux LAPTOP-HFSOU9IB 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  10:50:54 up 46 min,  0 users,  load average: 0.01, 0.07, 0.05

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -6,10 +6,8 @@ static void os_init() {
 }
 
 static void os_run() {
-  for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
-    putch(*s == '*' ? '0' + cpu_current() : *s);
-  }
-  // while (1) ;
+  void * p=pmm->alloc(10000);
+  pmm->free(p);
 }
 
 MODULE_DEF(os) = {
