b5e67b14f820e395774cfcf1820fec9ee4386bb2
tracer-nju <tracer@nju.edu.cn>
2023-04-13 19:41:39 +0800

    Linux LAPTOP-HFSOU9IB 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  19:41:39 up  2:25,  0 users,  load average: 0.15, 0.12, 0.07

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -51,7 +51,7 @@ static void os_init() {
 
 static void os_run() {
   int cpu = cpu_current();
-  printf("%d",cpu);
+  printf("%d",6);
   atom_printf("os_run() from %d CPU", cpu);
   iset(true);// 开中断
   while (1);
