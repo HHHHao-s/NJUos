6acc7ef3f0815fe43bbfc4c898fbca98aea660e1
tracer-nju <tracer@nju.edu.cn>
2023-04-15 11:44:45 +0800

    Linux LAPTOP-HFSOU9IB 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  11:44:45 up  1:05,  0 users,  load average: 0.07, 0.14, 0.08

--- kernel/framework/main.c
+++ kernel/framework/main.c
@@ -3,13 +3,9 @@
 
 int main() {
   ioe_init();
-  char buf[4];
-
-  snprintf(buf, 4, "%d", 123456);
-  printf(buf);
   cte_init(os->trap);
-  dev->init();
   os->init();
+  dev->init();
   mpe_init(os->run);
   
   return 1;
