844d0a5c4c3dd35e734e38614a966d34ede3c378
tracer-nju <tracer@nju.edu.cn>
2023-04-15 14:22:42 +0800

    Linux LAPTOP-HFSOU9IB 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:22:42 up  3:43,  0 users,  load average: 0.21, 0.18, 0.16

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -150,7 +150,7 @@ static Context *yield_handler(Event ev, Context *ctx){
       
       kmt->spin_unlock(&current_task->lock);
     }  
-    atom_printf(" from%s\n", current_task->name);      
+    atom_printf("from%s\n", current_task->name);      
   }
   else {
     current_task = task_list.head;
@@ -166,7 +166,7 @@ static Context *yield_handler(Event ev, Context *ctx){
     }while (current_task->status!=RUNNABLE);
     current_task->status=RUNNING;
     kmt->spin_unlock(&task_list.lock);
-    atom_printf(" turn to%s\n", current_task->name);
+    atom_printf("turn to%s\n", current_task->name);
     return current_task->context;
   } 
   
