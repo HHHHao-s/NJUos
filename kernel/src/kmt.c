530a16b8dd394527a88974fdc54c118ee7ed711a
HHHHao-s <2755942193@qq.com>
2023-04-19 18:30:42 +0800

    M3 init

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -1,14 +1,5 @@
 #include <os.h>
 
-// void (*init)();
-// int  (*create)(task_t *task, const char *name, void (*entry)(void *arg), void *arg);
-// void (*teardown)(task_t *task);
-// void (*spin_init)(spinlock_t *lk, const char *name);
-// void (*spin_lock)(spinlock_t *lk);
-// void (*spin_unlock)(spinlock_t *lk);
-// void (*sem_init)(sem_t *sem, const char *name, int value);
-// void (*sem_wait)(sem_t *sem);
-// void (*sem_signal)(sem_t *sem);
 
 static void kmt_spin_init(spinlock_t *lk, const char *name);
 static void kmt_spin_lock(spinlock_t *lk);
