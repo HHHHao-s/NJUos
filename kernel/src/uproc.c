b278ccab415cb715da6ab027ccb1a36589cd9001
tracer-nju <tracer@nju.edu.cn>
2023-04-28 12:40:06 +0800

    Linux LAPTOP-HFSOU9IB 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  12:40:06 up  1:10,  0 users,  load average: 0.08, 0.02, 0.01

--- kernel/src/uproc.c
+++ kernel/src/uproc.c
@@ -23,12 +23,13 @@ int uinit(task_t *task, const char *name, void (*entry)(void *arg), size_t len){
     map(&task->as, begin,place, MMAP_READ|MMAP_WRITE);
 
     void *pstack = pmm->alloc(task->as.pgsize);
-    void *vstack = task->as.area.end-task->as.pgsize;
     uint64_t vrsp = (uint64_t)task->as.area.end;
+    void *vstack = (void *)vrsp-task->as.pgsize;
+    
     map(&task->as, vstack, pstack, MMAP_READ|MMAP_WRITE);
 
     task->context = ucontext(&task->as ,(Area){.start=&task->fence + 1,.end=task+1},begin);
-    task->context->rsp = vrsp;
+    task->context->rsp = vrsp-16; // 防止指针超出栈
     task->entry = begin;
 
     strncpy(task->name, name, KMT_NAME_SIZE);
