788f844844ace65e4c74fc5d8b6cc848e3dc2b93
tracer-nju <tracer@nju.edu.cn>
2023-04-30 14:06:20 +0800

    Linux LAPTOP-HFSOU9IB 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:06:20 up  3:06,  0 users,  load average: 0.21, 0.15, 0.05

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -205,6 +205,7 @@ static Context *inter_handler(Event ev, Context *ctx){
   
   yield(); // 将执行到这里的状态保存起来，待调用
   // ctx->cr3 = current_task->as.ptr;
+  atom_printf("%d",current_task->id);
   atom_printf("%p\n",ctx->cr3);
   return ctx;
 
