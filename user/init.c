e676cc7c7cfea837638528692d79d8f32b11839b
HHHHao-s <2755942193@qq.com>
2023-05-01 13:49:57 +0800

    fork complete

--- user/init.c
+++ user/init.c
@@ -6,12 +6,36 @@ int main() {
   // Example:
   // char *pfar = "far\n";
   // char *pson =  "son\n";
+  volatile int a[12] = {-1,-1};
   int id;
-  if((id=syscall(SYS_fork,0,0,0,0))==0){ // son
-    syscall(SYS_kputc, id+'0',0,0,0);
-  }else{
-    syscall(SYS_kputc, id+'0',0,0,0);
+  
+
+  for(int i=0;i<3;i++){
+    if((id=syscall(SYS_fork,0,0,0,0))==0){ // son
+      a[0] = i;
+      while(1){
+        syscall(SYS_kputc, a[0]+'0',0,0,0);
+        for(int j=0;j<100000;j++); // spin
+        }    
+    }
   }
+
+
+  // if((id=syscall(SYS_fork,0,0,0,0))==0){ // son
+  //   a[0] = 's';
+  //   while(1){
+  //     syscall(SYS_kputc, a[0],0,0,0);
+  //     for(int j=0;j<100000;j++); // spin
+  //     }
+  //   }else{
+  //     a[0] = 'f';
+  //     while(1){
+  //       syscall(SYS_kputc, a[0],0,0,0);
+  //       for(int j=0;j<100000;j++); // spin
+  //     }
+  //   }
+      
+  
   
     
   while(1);
