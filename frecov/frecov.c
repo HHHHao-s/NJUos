88709e342a50291139f731483cdd69c9c2e8b06c
tracer-nju <tracer@nju.edu.cn>
2023-05-05 09:57:37 +0800

    Linux LAPTOP-HFSOU9IB 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  09:57:37 up 3 min,  0 users,  load average: 0.08, 0.14, 0.07

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -203,7 +203,9 @@ void doit(u8 *whole_disk){
     if(((uintptr_t)entry-(uintptr_t)whole_disk) == 0x25040){
       retrive_long(entry);
     }
-    printf("%lx",((uintptr_t)entry-(uintptr_t)whole_disk));
+    printf("\n%x\n",(unsigned int)((uintptr_t)entry-(uintptr_t)whole_disk));
+  
+  
     entry++;
   }
   printf("\n%x\n",(unsigned int)((uintptr_t)entry-(uintptr_t)whole_disk));
